fc874b57260ff8cad78ecc6a1fbe1f86ca8b3dc1
arnoldb1778 <arnoldb1778@gmail.com>
2020-09-03 15:43:58 -0700

    add another print line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello, this is GitHub2");
+        System.out.println("another print line");
     }
 }
