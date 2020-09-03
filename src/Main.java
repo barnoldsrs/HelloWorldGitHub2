3fe64c7599505b1f1998a09ea8466a85fd92da14
barnoldsrs <64119435+barnoldsrs@users.noreply.github.com>
2020-09-03 15:59:07 -0700

    Add exclamation marks.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello, this is GitHub2");
-        System.out.println("another print line");
-        System.out.println("A third line here");
+        System.out.println("another print line!!!");
+        System.out.println("A third line here!!!");
     }
 }
